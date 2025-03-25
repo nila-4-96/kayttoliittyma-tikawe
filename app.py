6a5ae8d9fb62e141adfa96f3a7d474f61228ac15
Litja Niko S <niklitja@cubbli24-gold.cs.helsinki.fi>
2025-03-25 11:55:02 +0200

    Added template index.html, added pages to app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Tähän tulee käyttöliittymä."
+    return render_template("index.html")
 
 @app.route("/page1")
-def index():
-    return "Tämä on sivu 1"
+def page1():
+    return "Tämä lienee sivu 1"
 
 @app.route("/page2")
-def index():
-    return "Tämä on sivu 2"
+def page2():
+    return "Tämä lienee sivu 2"
+
+@app.route("/test")
+def test():
+    content = ""
+    for i in range(1, 101):
+        content += str(i) + " "
+    return content
+
+@app.route("/page/<int:page_id>")
+def page(page_id):
+    return "Tämä on sivu " + str(page_id)
