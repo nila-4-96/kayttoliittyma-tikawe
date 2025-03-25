4775112e0daea5da1528aaab8173ad4a9332c44c
Litja Niko S <niklitja@cubbli24-gold.cs.helsinki.fi>
2025-03-25 10:38:52 +0200

    Added page 1 and page 2

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Tähän tulee käyttöliittymä."
+
+@app.route("/page1")
+def index():
+    return "Tämä on sivu 1"
+
+@app.route("/page2")
+def index():
+    return "Tämä on sivu 2"
